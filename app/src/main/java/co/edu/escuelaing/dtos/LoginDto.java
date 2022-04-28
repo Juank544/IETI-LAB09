2ea71c5f5a55fe11a25949d110a26fccff7c600f
Juank544 <camilo.gv@live.com>
2022-04-28 18:50:51 -0500

    Dtos

---
+++ app/src/main/java/co/edu/escuelaing/dtos/LoginDto.java
@@ -0,0 +1,19 @@
+package co.edu.escuelaing.dtos;
+
+public class LoginDto {
+    public String email;
+    public String password;
+
+    public LoginDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ app/src/main/java/co/edu/escuelaing/dtos/TokenDto.java
@@ -0,0 +1,21 @@
+package co.edu.escuelaing.dtos;
+
+import java.util.Date;
+
+public class TokenDto {
+    private String token;
+    private Date expirationDate;
+
+    public TokenDto(String token, Date expirationDate) {
+        this.token = token;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+}
